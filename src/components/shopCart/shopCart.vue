c192048f0b9a6dbe1ba21f986f9b5988cd802171
张韧 <zhangr@ethicall.cn>
2016-12-27 21:15:39 +0800

    update

--- src/components/shopCart/shopCart.vue
+++ src/components/shopCart/shopCart.vue
@@ -30,14 +30,20 @@ export default {}
 
 <style lang="stylus">
 .shopCart
+  position fixed
+  bottom 0
+  background #141d27
+  width 100%
+  height 46px
   .content
-    position fixed
-    bottom 0
-    background #141d27
-    width 100%
-    height 46px
+    display flex
     .content-left
-      /*padding-left 18px*/
+      flex 1
+      height 46px
       .logo-wrapper
-
+        height 44px
+        border-radius 50%
+        width 10px
+    .content-right
+      flex 0 0 105px
 </style>
