5c528776e8e9350635aea079bbbe2b4cf97c27a8
张韧 <zhangr@ethicall.cn>
2017-01-06 23:06:50 +0800

    add foodDetail

---
+++ src/components/foodDetail/foodDetail.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+<div class="foodDetail">
+  <div class="img">
+
+  </div>
+  <div class="info">
+    <div class="title">
+
+    </div>
+    <div class="desc">
+
+    </div>
+    <div class="price">
+
+    </div>
+    <div class="shopCart">
+
+    </div>
+  </div>
+  <div class="divider"></div>
+  <div class="desc">
+
+  </div>
+  <div class="divider"></div>
+  <div class="evaluation">
+    <div class="classify">
+
+    </div>
+    <div class="evel-list">
+      <ul>
+        <li class="evel"></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+
+</script>
+
+<style lang="stylus">
+
+</style>
