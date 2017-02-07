2aa0322c8c13273673c03a20daef4156d7ba7627
张韧 <zhangr@ethicall.cn>
2017-02-07 17:56:27 +0800

    detail done

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -2,14 +2,14 @@
 
   <div class="cartcontrol">
     <transition name="fadeRotate">
-      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart()">
+      <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart()">
           <span class="icon-remove_circle_outline inner"></span>
       </div>
     </transition>
     <div class="cart-count" v-show="food.count>0">
       {{food.count}}
     </div>
-    <div class="cart-add" @click="addCart($event)">
+    <div class="cart-add" @click.stop.prevent="addCart($event)">
       <i class="icon-add_circle"></i>
     </div>
   </div>
