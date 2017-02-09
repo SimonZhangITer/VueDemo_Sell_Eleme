ebceb299450a560267fe607f7f61cbfac268d8ca
张韧 <zhangr@ethicall.cn>
2017-02-09 15:05:33 +0800

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,9 @@
       <router-link to="/seller">商家</router-link>
     </div>
   </div>
-  <router-view :seller="seller"></router-view>
+  <keep-alive>
+    <router-view :seller="seller"></router-view>
+  </keep-alive>
 </div>
 
 </template>
