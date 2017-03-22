8f9b0195256859342c3c06e1ec30e30c3e7e787a
张韧 <zhangr@ethicall.cn>
2017-03-22 17:10:39 +0800

    github pages

--- src/filter/time.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import moment from 'moment'
-
-Vue.filter('time', function(value, formatString) {
-  formatString = formatString || 'YYYY-MM-DD HH:mm';
-  return moment(value).format(formatString);
-})
