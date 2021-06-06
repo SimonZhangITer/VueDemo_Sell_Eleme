858542963c4e3c8baea6c459bd480d21d2b2f563
gfzhujie <gfzhujie@gf.com.cn>
2021-06-06 21:15:08 +0800

    JXKHXT-269:  干部档案系统----修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,4 @@
-<style lang="stylus" rel="stylesheet/stylus">
-  @import 'common/stylus/index'
-  .tab
-    display:flex
-    width:100%
-    height:40px
-    line-height:40px
-    border-1px(rgba(7,17,27,0.1))
-    .tab-item
-      flex:1
-      text-align:center
-      a
-        display:block
-        font-size:14px
-        color rgb(77,85,93)
-        &.active
-          font-size 14px
-          color rgb(240,20,20)
-</style>
+
 
 <template>
 <div>
@@ -62,3 +44,24 @@ export default {
 }
 
 </script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import 'common/stylus/index'
+.tab
+  display:flex
+  width:100%
+  height:40px
+  line-height:40px
+  border-1px(rgba(7,17,27,0.1))
+  .tab-item
+    flex:1
+    text-align:center
+    a
+      display:block
+      font-size:14px
+      color rgb(77,85,93)
+      &.active
+        font-size 14px
+        color rgb(240,20,20)
+</style>
